32db4db3cee2e7b7c097dabaf66c4b7ea9e2a78b
akash <md.mostafa.akash@gmail.com>
2023-04-03 17:29:34 +0600

    fixed typos

--- check.js
+++ check.js
@@ -1,18 +1,2 @@
-import { createRandomUser, generateNumber, getEmail, getName, getPhone} from "./utils/randomUtils.js";
-import { getRandomUser, getTwoCustomersFromFile, saveUserToJson } from './utils/utils.js';
-import userData from './testdata/users.json' assert {type : "json"};
-import configData from '../env.json' assert { type : "json" };
-import fs from 'fs';
 
-
-function getRandomUserFromFile(){
-    var randomNumber = generateNumber(0, userData.length);
-    return userData[randomNumber]
-
-}
-
-let x = getRandomUser("Customer", true);
-console.log(x);
-
-
-
+console.log("This is for checking")
